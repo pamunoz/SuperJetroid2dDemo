557649af563747945403950fb1a748c05d2b1bb1
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-11 10:51:14 -0400

    Add working with properties

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,31 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
 
 public class HelloWorld : MonoBehaviour {
 
+	// for this case we use the type,instead of var
+	public List<string> list = new List<string> ();
+	public float speed = 2f;
+
 	// Use this for initialization
 	void Start () {
-		// if the type is ovious is better to use var, instead of the type;
-		var name = "Pablo Munoz";
-		var age = 33;
-		var speed = 4.3f;
-		var likesGames = true;
+		list.Add ("Hello");
+		list.Add ("World");
 
-		var stringArray = new string[2];
-		stringArray[0] = "Hello";
-		stringArray[1] = "World";
-
-		var phrase = stringArray [0] + " " + stringArray [1];
-
-		// for showing something in the console we can use Debug.Log
-		Debug.Log(phrase);
-		// Or we can use pring
-		print(name);
+		print (list [0] + " " + list [1]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate (new Vector3 (speed, 0, transform.position.z) * Time.deltaTime);	
+	
 	}
 }
