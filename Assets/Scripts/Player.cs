e448c40cc78c53f8610baff717d32ac1c4cd6ff0
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-12 18:52:49 -0400

    Add moving up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿﻿using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour {
 
 	public float speed = 10f;
-	public Vector2 maxVelocity = new Vector2 (3, 5);
-
+	public Vector2 maxVelocity = new Vector2(3, 5);
+	public bool standing;
+	public float jetSpeed = 15f;
+	public float airSpeedMultiplier = .3f;
 	Rigidbody2D rigidbody2D;
 
 	void Start() {
-		rigidbody2D = GetComponent<Rigidbody2D> ();
+		rigidbody2D = GetComponent<Rigidbody2D> (); 
 	}
 
-	
 	// Update is called once per frame
 	void Update () {
 		var forceX = 0f;
 		var forceY = 0f;
 
 		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		var absVelY = Mathf.Abs (rigidbody2D.velocity.y);
+
+		if (absVelY < .2f) {
+			standing = true;
+		} else {
+			standing = false;
+		}
 
 		if (Input.GetKey ("right")) {
-			if (absVelX < maxVelocity.x)
-				forceX = speed;
 
-			// We want to flip the player left to right
+			if(absVelX < maxVelocity.x)
+				forceX = standing ? speed : (speed * airSpeedMultiplier);
+
 			transform.localScale = new Vector3(1, 1, 1);
-			
-		} else if (Input.GetKey ("left")) {
-			if (absVelX < maxVelocity.x)
-				forceX = -speed;
 
-			// We want to flip the player left to right
+		} else if (Input.GetKey ("left")) {
+
+			if(absVelX < maxVelocity.x)
+				forceX = standing ? -speed : (-speed * airSpeedMultiplier);
+
 			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetKey ("up")) {
+			if(absVelY < maxVelocity.y)
+				forceY = jetSpeed;
 		}
 
 		rigidbody2D.AddForce (new Vector2 (forceX, forceY));
