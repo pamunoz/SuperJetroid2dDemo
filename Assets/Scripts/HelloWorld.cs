e7db81c1e82634d26e29135ed3e1c6c4d2710ed2
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-12 16:54:14 -0400

    Add player collition

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,42 +1,25 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class HelloWorld : MonoBehaviour {
 
-	public List<string> list = new List<string>();
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Security.Cryptography;
-
-public class HelloWorld : MonoBehaviour {
-
 	// for this case we use the type,instead of var
 	public List<string> list = new List<string> ();
->>>>>>> 1caab0d3813b6522b02840e3af59e129bc8b50b4
 	public float speed = 2f;
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-=======
 		list.Add ("Hello");
 		list.Add ("World");
 
->>>>>>> 1caab0d3813b6522b02840e3af59e129bc8b50b4
 		print (list [0] + " " + list [1]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
 		transform.Translate (new Vector3 (speed, 0, transform.position.z) * Time.deltaTime);
-=======
 		transform.Translate (new Vector3 (speed, 0, transform.position.z) * Time.deltaTime);	
 	
->>>>>>> 1caab0d3813b6522b02840e3af59e129bc8b50b4
 	}
 }
