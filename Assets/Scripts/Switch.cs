3dc0fefef5fd5f5870559a3e7f788c1426c85cba
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-13 17:59:10 -0300

    Add creating switches

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D target) {
+		int switchDown = 1;
+		animator.SetInteger ("AnimState", switchDown);		
+	}
+
+	void OnTriggerExit2D(Collider2D target) {
+		int switchUp = 2;
+		animator.SetInteger ("AnimState", switchUp);
+	}
+}
