c8490a81d8145a9cc46e9b6c2898597fa65c276b
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-13 11:06:51 -0300

    Add Collectable objects

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// This method is gonna return a collider 2d
+	// that represent the target of the object that
+	// set up the trigger
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.gameObject.tag == "Player")
+			Destroy (gameObject);		
+	}
+}
