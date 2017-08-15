86d375b389e9f04fd50b1fb1d3005518f088f6af
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-15 19:53:05 -0300

    Set up the camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// This will allow us to follow any game object we set to it.
+	public GameObject target;
+
+	// Reference to the camera
+	private Camera mCamera;
+
+	void Awake() {
+		// this is to set the camera 
+		// to pixel perfect size
+		float pixelRatio = 100f;
+		mCamera = GetComponent<Camera>();
+		mCamera.orthographicSize = ((Screen.height / 2.0f) / pixelRatio);
+	}
+
+	/*
+	 * For the camera, which updates constantly in every frame,
+	 * it's easy to catch a reference to this transform,
+	 * so we don't have a performance hit.
+	 */ 
+	private Transform _t;
+
+	// Use this for initialization
+	void Start () {
+		_t = target.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Set the camera's position, to the target position
+		// while maintaining the camera z index
+		transform.position = new Vector3(_t.position.x, _t.position.y, transform.position.z);
+		
+	}
+}
