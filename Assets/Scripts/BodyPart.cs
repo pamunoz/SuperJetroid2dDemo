6e8eb39ce1a275af5dd9f94f757b28cec06450d1
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-15 12:46:52 -0300

    Add Exploding player

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour {
+
+	private SpriteRenderer mSpriteRenderer;
+	private Color mColorStart;
+	private Color mColorEnd;
+	private float mTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		mSpriteRenderer = GetComponent<SpriteRenderer> ();
+		mColorStart = mSpriteRenderer.color;
+		float alpha = 0.0f;
+		mColorEnd = new Color (mColorStart.r, mColorStart.g, mColorStart.b, alpha);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FadesOutBodyPart ();		
+	}
+
+	/* 
+	 * This methods will make the body part fades out and destroy itself
+	 */
+	private void FadesOutBodyPart() {
+		// We gonna change the material clor over time
+		mTime += Time.deltaTime;
+		// Lerp interpolate over two values over a fraction of time
+		mSpriteRenderer.material.color = Color.Lerp (mColorStart, mColorEnd, mTime / 2);
+		// alpha is let than cero
+		float currentAlpha = mSpriteRenderer.material.color.a;
+		// if the body part is transparent, it destroy itself
+		if (currentAlpha <= 0.0) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour {
 
+	public BodyPart bodyPart;
+	public int totalParts = 5;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () {}
 	// This method is trigger on the object colliding with another object
 	void OnTriggerEnter2D(Collider2D target) {
 		if (target.gameObject.tag == "Deadly") {
@@ -23,5 +22,40 @@ public class Explode : MonoBehaviour {
 	void OnExplode() {
 		// This will remove the player from the Scene
 		Destroy (gameObject);
+		
+		// generate random body parts with random directions
+		GenerateBodyParts ();
+	}
+
+	/* This methods generates body parts and apply forces to them in different directions */
+	private void GenerateBodyParts() {
+		
+
+		var currentTransform = transform;
+
+		for (int i = 0; i < totalParts; i++) {
+			// this move the position of the body part to start outside of the player area
+			currentTransform.TransformPoint (0, -100, 0);
+			// then a new version of the body part is cloned from the prefab folder
+			//Vector3 newPosition = currentTransform.position;
+			//Quaternion zeroRotation = Quaternion.identity;
+			BodyPart clone = new BodyPart();
+			if (bodyPart != null) {
+				clone = Instantiate (bodyPart, currentTransform.position, Quaternion.identity) as BodyPart;
+			} else {
+				Debug.Log ("This objects is null, what is happening!!!");
+			}
+
+			// We apply a force to the rigidbody
+			Rigidbody2D currentRigidBody = clone.GetComponent<Rigidbody2D>();
+
+			if (currentRigidBody != null) {
+				// apply force going left or righ
+				currentRigidBody.AddForce (Vector3.right * Random.Range (-50, 50));
+				// apply force going up in a specific speed
+				currentRigidBody.AddForce (Vector3.up * Random.Range (100, 400));				
+			}
+
+		}		
 	}
 }
