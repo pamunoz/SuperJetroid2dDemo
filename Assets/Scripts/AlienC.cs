33dd43d0bd4f35fbf45f762203e8e7dc8a569322
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-15 18:09:11 -0300

    Add Enabling aliens to shoot

---
+++ Assets/Scripts/AlienC.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienC : MonoBehaviour {
+
+	public float attackDelay = 3f;
+
+	private Animator mAnimator;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start...");
+		mAnimator = GetComponent<Animator> ();
+
+		// if the attack delay is 0 or less, the alien wont attack
+		if (attackDelay > 0) {
+			StartCoroutine (OnAttack());			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int idle = 0;
+		mAnimator.SetInteger ("AnimState", idle);		
+	}
+
+	IEnumerator OnAttack() {
+		Debug.Log ("OnAttack...");
+		yield return new WaitForSeconds (attackDelay);
+		Fire ();
+		StartCoroutine (OnAttack ());
+	}
+
+	void Fire() {
+		Debug.Log ("Fire...");
+		int attack = 1;
+		// Change the animation to the attack state
+		mAnimator.SetInteger ("AnimState", attack);
+	}
+}
