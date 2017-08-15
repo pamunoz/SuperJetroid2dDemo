d8fa42a4baaec7c6acbcb0a7fcb4cdaec128e46b
Pablo Munoz <pfariasmunoz@gmail.com>
2017-08-15 16:50:05 -0300

    Add Alien Attack

---
+++ Assets/Scripts/AlienB.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienB : MonoBehaviour {
+
+	private Animator mAnimator;
+
+	// Use this for initialization
+	void Start () {
+		mAnimator = GetComponent<Animator> ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// if something collide with this
+	// it will atack
+	void OnTriggerEnter2D(Collider2D target) {
+		mAnimator.SetInteger ("AnimState", 1);
+	}
+}
